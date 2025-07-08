05ceaa55399761b137e7af7708a4c16ccacf7113
Sanghoon Lee <gcccompil3r@gmail.com>
2025-07-08 17:03:38 +0900

    feat: 회원 가입 및 로그인

--- spring/demo/src/main/java/com/example/demo/controller/UserController.java
+++ spring/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.controller.request.SignInRequest;
 import com.example.demo.controller.request.SignUpRequest;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -21,5 +24,26 @@ public class UserController {
 
         User user = request.toUser();
         return userRepository.save(user);
+    }
+
+    @PostMapping("/sign-in")
+    public Boolean signIn(@RequestBody SignInRequest request) {
+        log.info("signIn() -> request: {}", request);
+
+        String userId = request.getUserId();
+        Optional<User> maybeUser = userRepository.findByUserId(userId);
+        if (maybeUser.isEmpty()) {
+            log.info("signIn() -> 아이디 없음.");
+            return Boolean.FALSE;
+        }
+        User user = maybeUser.get();
+        String encryptedUserPassword = user.getPassword();
+        Boolean loginSuccess = request.checkPassword(encryptedUserPassword);
+        if (!loginSuccess) {
+            log.info("signIn() -> 암호 불일치.");
+            return Boolean.FALSE;
+        }
+
+        return Boolean.TRUE;
     }
 }
